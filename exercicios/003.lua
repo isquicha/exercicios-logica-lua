4df9d3e35d13afc710df9976162c64c1b1b80b59
Andre Vicente <isquicha@gmail.com>
2020-11-03 02:07:16 -0300

    Add exercicio 3

---
+++ exercicios/003.lua
@@ -0,0 +1,8 @@
+--[[
+Faça um Programa que peça dois números e imprima a soma.
+--]]
+print("Digite um número: ")
+numero1 = tonumber(io.read())
+print("Digite outro número: ")
+numero2 = tonumber(io.read())
+print("A soma é: " .. (numero1 + numero2))
