f168c3eca0b8ee6afa44374dd4df4e3db02e60cc
Andre Vicente <isquicha@gmail.com>
2020-11-03 02:07:15 -0300

    Add exercicio 2

---
+++ exercicios/002.lua
@@ -0,0 +1,7 @@
+--[[
+Faça um Programa que peça um número e então mostre a mensagem O número
+informado foi [número]. 
+--]]
+print("Digite um numero: ")
+numero = tonumber(io.read())
+print("O número informado foi " .. numero)
