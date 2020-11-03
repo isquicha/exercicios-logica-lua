15417e25794be9b4f086e7da45928ed775dbd0e2
Andre Vicente <isquicha@gmail.com>
2020-11-03 02:07:17 -0300

    Add exercicio 5

---
+++ exercicios/005.lua
@@ -0,0 +1,6 @@
+--[[
+Faça um Programa que converta metros para centímetros.
+--]]
+print("Digite o tamanho em metros: ")
+metros = tonumber(io.read())
+print(metros .. " metros equivalem a " .. (metros * 100) .. " centímetros")
