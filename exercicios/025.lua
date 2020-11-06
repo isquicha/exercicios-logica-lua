8b4ee6a55f3cd08332aa541497aca39b2ff34aa5
Andre Vicente <isquicha@gmail.com>
2020-11-06 10:21:35 -0300

    Add exercicio 25

---
+++ exercicios/025.lua
@@ -0,0 +1,24 @@
+--[[
+Faça um Programa que leia três números e mostre o maior e o menor deles.
+--]]
+print("Digite um numero: ")
+numero1 = tonumber(io.read())
+print("Digite outro numero: ")
+numero2 = tonumber(io.read())
+print("Digite mais um numero: ")
+numero3 = tonumber(io.read())
+if numero1 > numero2 and numero1 > numero3 then
+    print(numero1 .. " foi o maior numero digitado.")
+elseif numero2 > numero1 and numero2 > numero3 then
+    print(numero2 .. " foi o maior numero digitado.")
+else
+    print(numero3 .. " foi o maior numero digitado.")
+end
+ 
+if numero1 < numero2 and numero1 < numero3 then
+    print(numero1 .. " foi o menor numero digitado.")
+elseif numero2 < numero1 and numero2 < numero3 then
+    print(numero2 .. " foi o menor numero digitado.")
+else
+    print(numero3 .. " foi o menor numero digitado.")
+end
